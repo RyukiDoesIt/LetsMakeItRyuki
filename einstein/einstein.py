e2486ac042573b4771ffeac0c60cdd56cd2ea269
Ryuki <156128187+RyukiDoesIt@users.noreply.github.com>
2024-01-22 16:39:23 +0530

    added einstein question

---
+++ einstein/einstein.py
@@ -0,0 +1,9 @@
+# Given
+c = 300000000
+
+# User's i/p of mass
+mass = int(input("Enter mass in kg: "))
+
+# Convert to energy
+energy = mass * pow(c, 2)
+print("Energy equivalent to mass:", energy)
