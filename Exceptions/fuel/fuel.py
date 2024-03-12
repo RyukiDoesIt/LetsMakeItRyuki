68e1411efba33244316222865dc76fcd4e2517f7
Shaon Samanta <156128187+RyukiDoesIt@users.noreply.github.com>
2024-03-13 00:42:22 +0530

    Added initial code fuel.py

--- Exceptions/fuel/fuel.py
+++ Exceptions/fuel/fuel.py
@@ -1,13 +1,28 @@
-while True:
-    # Trying the code
-    try:
-        # Taking input
-        x = int(input("What's x? "))
-        # If ValueError is False
-        break
-    except ValueError:
-        # If ValueError is True
-        print("x is not an integer!")
-    
-# Print the variable
-print(f"x is {x}")
+def main():
+    percentage()
+
+def percentage():
+    while True:
+        try:
+            fraction = input("Fraction: ")
+            a, b = fraction.split("/")
+            ans = int(round((100*(int(a) / int(b)))))
+            if 95 < ans <= 100:
+                print("F")
+                break
+            elif ans < 10:
+                print("E")
+                break
+            elif ans > 100:
+                pass
+            else:
+                print(f"{ans}%")
+                break
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
+        except NameError:
+            pass
+
+main()
